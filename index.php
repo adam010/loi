61b4e9b9cef7e7a30f3f54fa59e79cbec7fa410a
Administrator <Administrator@server.jamama>
2013-10-02 23:11:12 +0200

    leden naw gegevens gereed

--- index.php
+++ index.php
@@ -2,17 +2,30 @@
 require_once( "classes/class.siteform.php" );
 require_once( "classes/class.leden.php" );
 @session_start();
-$lid=new leden;
-$fields= get_object_vars($lid);
+$registrant=new leden;
+$fields= get_object_vars($registrant);
 $regform=new siteform("registratie");
 $regform->formfields=$fields;
 
 //    var_dump($_POST);
 if(isset($_POST['thisform'])){
+    
     array_shift($_POST);
     array_pop($_POST);
   if($regform->processrequest($_POST))
-     $lid->register($regform->formfields);
+      //validate ok!
+     if($_POST['thisform']=="registratie"){ 
+         if(!isset($_SESSION['registrant']))
+            $_SESSION['lid']=$regform->formfields;     
+            $lidmaatschap=new lidmaatschap();
+            $fields= get_object_vars($lidmaatschap);
+            $regform=new siteform("lidmaatschap");
+            $regform->formfields=$fields;
+         
+     }
+     else if($_POST['thisform']=="lidmaatschap"){
+         
+     }
    
 }
 
