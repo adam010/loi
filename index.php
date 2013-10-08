248b7cf97e05a58a8e36c2c6858ee96375a1aec7
Administrator <Administrator@server.jamama>
2013-10-08 16:05:54 +0200

    leden naw gegevens gereed

--- index.php
+++ index.php
@@ -1,36 +1,63 @@
 <?php
 require_once( "classes/class.siteform.php" );
+require_once( "classes/class.lidmaatschap.php" );
 require_once( "classes/class.leden.php" );
 @session_start();
-$registrant=new leden;
-$fields= get_object_vars($registrant);
+$lid=new leden;
+$lidmaatschap=new lidmaatschap();
+$fields= $lid->nawformfields();
 $regform=new siteform("registratie");
 $regform->formfields=$fields;
 
-//    var_dump($_POST);
+
 if(isset($_POST['thisform'])){
-    
-    array_shift($_POST);
-    array_pop($_POST);
-  if($regform->processrequest($_POST))
+  $form= $_POST['thisform'];
+  array_shift($_POST);
+  array_pop($_POST);
+  if($regform->processrequest($_POST))      {
       //validate ok!
-     if($_POST['thisform']=="registratie"){ 
-         if(!isset($_SESSION['registrant']))
-            $_SESSION['lid']=$regform->formfields;     
-            $lidmaatschap=new lidmaatschap();
-            $fields= get_object_vars($lidmaatschap);
-            $regform=new siteform("lidmaatschap");
-            $regform->formfields=$fields;
-         
+     if($form=="registratie"){ 
+         if(!isset($_SESSION['aspirantlid']))
+            $_SESSION['aspirantlid']=$regform->formfields;                
+            
+        $fields= $lidmaatschap->formfields();
+        $regform=new siteform("lidmaatschap");
+        $regform->formfields=$fields;      
      }
-     else if($_POST['thisform']=="lidmaatschap"){
-         
+     if($form=="lidmaatschap" ){
+       $lidmaatschap->update($_POST);
+       $lid->save($_SESSION['aspirantlid']);
+       $lidmaatschap->lidnummer=$lid->id; 
+       if ($lidmaatschap->save())
+             echo "succes";          
+       
      }
-   
+      
+  }
+//    if($form=="registratie")
+//        $fields= $lid->nawformfields();
+//      else if($form=="lidmaatschap" )
+//       $fields= $lidmaatschap->formfields();   
+        
+else{$fields=$_POST;}
+           
+           
+            
 }
+     
+       
+
+   
+//     else if($_POST['thisform']=="lidmaatschap"){     
+//      $lidmaatschapDetails=$regform->formfields;   
+//      $lidnummer=$lid->register($_SESSION['aspirantlid']);
+//      $lidmaatschapDetails->lidnummer=$lidnummer;
+//      $lidmaatschap->submit(litmaatschapDetails);
+//     }
+   
 
 
-
+//$regform->formfields=$fields;
 $regform->formHTML=$regform->getform($fields)
 ;?>
 <!DOCTYPE html>
