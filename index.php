567467e3aad46d8eb2670e6ac64309f73de71c9d
Administrator <Administrator@server.jamama>
2013-10-13 00:47:35 +0200

    Refractor en code format compleet

--- index.php
+++ index.php
@@ -2,72 +2,68 @@
 require_once( "classes/class.siteform.php" );
 require_once( "classes/class.lidmaatschap.php" );
 require_once( "classes/class.leden.php" );
+/*
+ *
+ * -------------------------------------------------------
+ * FILENAME: index
+ * FILE:     index.php
+ * TABLES :  leden,lidmaatschap
+ * MYSQL DB: dbloi
+ * -------------------------------------------------------
+ * Created by : D.M.Keteldijk
+ * project : LOI-inzendopdracht051R5
+ * -------------------------------------------------------
+ *
+ */
 @session_start();
 
-$lid=new leden;
-$lidmaatschap=new lidmaatschap();
-//
-    $regform=new siteform("registratie");
-    
 //init (naw) registratie
+$lid = new leden;
+$lidmaatschap = new lidmaatschap();
 
+//form generator
+$regform = new siteform("registratie");
 
 //process form
-if(isset($_POST['thisform'])){
-  $form= $_POST['thisform'];
-  //sanitize post  
-  array_shift($_POST);
-  array_pop($_POST);
-  
-  if($regform->processrequest($_POST))      {
-      //is valid form 
-     if($form=="registratie"){ 
-         if(!isset($_SESSION['aspirantlid']))
-            $_SESSION['aspirantlid']=$regform->formfields;                
+if (isset($_POST['thisform'])) {
+    $form = $_POST['thisform'];
+    
+    //sanitize post  
+    array_shift($_POST);//remove thisform elem.
+    array_pop($_POST); //remove html button elem.
+
+    if ($regform->processrequest($_POST)) {
+        //is valid form 
+        if ($form == "registratie") {
+            if (!isset($_SESSION['aspirantlid']))
+                $_SESSION['aspirantlid'] = $regform->formfields;
+
+            //naw ok, nu naar lidmaatshap form 
+            $regform = new siteform("lidmaatschap");
+            $regform->formfields = $lidmaatschap->formfields();
+            $regform->initForm();
+        }
+        if ($form == "lidmaatschap") {
+            //data to object
+            $lidmaatschap->update($_POST);
             
-        //naw ok, nu naar lidmaatshap form 
-        $regform=new siteform("lidmaatschap");
-        
-        $regform->formfields=$lidmaatschap->formfields();      
-     }
-     
-     if($form=="lidmaatschap" ){
-       //data to object
-       $lidmaatschap->update($_POST);
-       //save all
-       $lid->save($_SESSION['aspirantlid']);
-       $lidmaatschap->lidnummer=$lid->id; 
-       if ($lidmaatschap->save())
-           $mail_verzonden=$lid->sendemail($lidmaatschap->data());
-      
-     }
-      
-  }        
-  else{$fields=$_POST;}  
-
-   }// form posted
-else{    
-$regform->formfields=$lid->formfields();
+            //save all
+            $lid->save($_SESSION['aspirantlid']);
+            $lidmaatschap->lidnummer = $lid->id;
+            if ($lidmaatschap->save())
+                $mail_verzonden = $lid->sendemail($lidmaatschap->data());
+        }
+    } //errors displayed in formHTML 
+}// form posted
+else {
+    //standaard eerste scherm naw-registratie
+    $regform->formfields = $lid->formfields();
+    $regform->initForm();
 }
-$regform->initForm();       
-if(isset($mail_verzonden))    
-    $regform->formHTML=$mail_verzonden;
 
-
-
-   
-//     else if($_POST['thisform']=="lidmaatschap"){     
-//      $lidmaatschapDetails=$regform->formfields;   
-//      $lidnummer=$lid->register($_SESSION['aspirantlid']);
-//      $lidmaatschapDetails->lidnummer=$lidnummer;
-//      $lidmaatschap->submit(litmaatschapDetails);
-//     }
-   
-
-
-//$regform->formfields=$fields;
-//$regform->formHTML=$regform->getform($fields)
-;?>
+if (isset($mail_verzonden))
+    $regform->formHTML = $mail_verzonden;
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -75,8 +71,6 @@ if(isset($mail_verzonden))
         <title></title>
     </head>
     <body>
-        <?php        
-        echo $regform->formHTML
-        ?>
+        <?php echo $regform->formHTML ?>
     </body>
 </html>
